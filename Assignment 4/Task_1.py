849aedd0201ed504b541a274277fee94762e8118
Smarajit Karmakar <smarajitkarmakar920@gmail.com>
2025-08-30 00:18:17 +0530

    Added Assignment 4

---
+++ Assignment 4/Task_1.py
@@ -0,0 +1,13 @@
+# Write a Python program that:
+# 1.   Opens and reads a text file named sample.txt.
+# 2.   Prints its content line by line.
+# 3.   Handles errors gracefully if the file does not exist.
+try:
+    file = open(r"C:\Users\KIIT0001\Desktop\python\Tutedude Assignments\Assignment 4\Sample.txt", "r")
+    reading_file = file.read()
+    print(reading_file)
+    file.close()
+    print("The file exists")
+except FileNotFoundError:
+    print("The file does not exist")
+
